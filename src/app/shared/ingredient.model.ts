ff2de3231f7a0df397327beec95224ed35ddf3b8
Jake Stanger <stangbang27@gmail.com>
2018-08-18 10:20:59 -0600

    stopping point before databinding

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+  // public name: string;
+  // public amount: number;
+
+  // constructor(name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+}
